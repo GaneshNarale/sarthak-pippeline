ce033e207f9e6256440f6a51fa33db4486a41aae
Ganesh Narale <gnarale1232@gmail.com>
2026-02-21 15:16:34 +0530

    Update response body in lambda_handler

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,6 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('ganesh pipeline   ')
+        'body': json.dumps('ganesh pipeline  aa ')
     }
 
