a3bf5a3d1464525a20d9001e29e3238fde2aa372
Ganesh Narale <gnarale1232@gmail.com>
2026-02-21 16:33:22 +0530

    Fix typo in greeting message

--- lambda_function.py
+++ lambda_function.py
@@ -7,7 +7,7 @@ table = dynamodb.Table('LambdaLogs')
 
 def lambda_handler(event, context):
     
-    message = "Hello from DevOps to devops engineer "
+    message = "Hello from DevOps to devops engineerrrr "
     
     # Store in DynamoDB
     table.put_item(
